2f269e2b2bac0664be89cfa04b5f62dbc2bf7eb4
j-pettit <julian.s.pettit@gmail.com>
2019-11-13 13:11:14 -0500

    support multiple arg types

--- detect.py
+++ detect.py
@@ -1,2 +1,13 @@
-from subprocess import callable
-rc = call("./detect.sh")
+import argparse
+from subprocess import call
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", default="sentry", type=str, help="Desired Detection Mode")
+args = parser.parse_args()
+
+type = args.mode
+
+if mode == 'sentry':
+    rc = call("./detect.sh")
+else:
+    print('Error: Detection mode not implemented')
